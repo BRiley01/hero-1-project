3850b5b6c6061e8737d48bb19a9cdb938d063bad
Brian Riley <briley01@gmail.com>
2015-03-26 23:25:28 -0400

    Speech library now supports op codes

--- Pi/src/Apps/speechSynth.cpp
+++ Pi/src/Apps/speechSynth.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	Speech* sp;
+	int status;
 	try
 	{
 		sp = new Speech("dictionary.db");		
@@ -22,6 +23,36 @@ int main(int argc, char **argv)
 		getline(cin, phrase);
 		if(phrase != "q")
 			sp->Say(phrase.c_str());
+		status = sp->Status();
+		while(status != SPEECH_STATUS_READY)
+		{
+			switch(status)
+			{
+				case SPEECH_STATUS_READY: 
+					cout << "ready\n";
+					break;
+				case SPEECH_STATUS_LOADING:
+					cout << "loading\n";
+					break;
+				case SPEECH_STATUS_SPEECH_READY:
+					cout << "speech ready\n";
+					break;
+				case SPEECH_STATUS_SPEAKING:
+					cout << "speaking\n";
+					break;
+				case SPEECH_STATUS_WAIT_AR:
+					cout << "waiting on AR\n";
+					break;
+				case SPEECH_STATUS_ABORTING:
+					cout << "aborting\n";
+					break;
+				case SPEECH_STATUS_FAULT:
+					cout << "fault\n";
+					break;
+			}
+			//usleep(10000); //sleep for 1 millisecond
+			status = sp->Status();
+		}
 	}
 	delete sp;
 	
--- Pi/src/speech/speech.h
+++ Pi/src/speech/speech.h
@@ -4,6 +4,17 @@
 #include <sqlite3.h>
 
 #define I2C_SPEECH 0x04
+#define SPEECH_OPCODE_STATUS 0x00
+#define SPEECH_OPCODE_SAY 0x01
+#define SPEECH_OPCODE_ABORT 0xFE
+
+#define SPEECH_STATUS_READY 0x01
+#define SPEECH_STATUS_LOADING 0x02
+#define SPEECH_STATUS_SPEECH_READY 0x03
+#define SPEECH_STATUS_SPEAKING 0x04
+#define SPEECH_STATUS_WAIT_AR 0x05
+#define SPEECH_STATUS_ABORTING 0x06
+#define SPEECH_STATUS_FAULT 0xFF
 
 class Speech
 {
@@ -11,6 +22,7 @@ class Speech
 		sqlite3* _db;
 		int _i2cHandle;
 		void toUpper(std::string& s);
+		bool sendOpCode(int opcode);
 	protected:
 		std::vector<unsigned char> wordToPhonemeBytes(const char* word);
 		bool send(std::vector<unsigned char> bytes);
@@ -18,4 +30,6 @@ class Speech
 		Speech(const char* db);
 		~Speech();
 		bool Say(const char* phrase);
+		int Status();
+		void Abort();
 };
